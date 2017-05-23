395ab69ef978b6f6b51f49a55124f91dd1af8079
SandzoNight <flukemdc@gmail.com>
2017-05-23 22:20:42 +0700

    embeded jquery file version 2.1.1

--- src/js/fluke.js
+++ src/js/fluke.js
@@ -1,4 +1,17 @@
+var mydiv = document.getElementById("product1");
 function viewFlukeProduct(){
-    var mydiv = document.getElementById("product1");
-    mydiv.getElementsByClassName("modal-content")[0].innerHTML = "";
+    // mydiv.getElementsByClassName("modal-content")[0].innerHTML = "";
+    mydiv.getElementsByClassName("product-name")[0].innerHTML = "LIXHULT<hr>";
+    flukeGetImage();
+}
+
+function flukeGetImage(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            mydiv.getElementsByClassName("product-desc")[0].innerHTML = this.responseXML.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+        }
+    }
+    xhttp.open("GET","src/products.xml",true);
+    xhttp.send();
 }
