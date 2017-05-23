e07deb90a555326470da9d1b1f5bd70f58189e91
SandzoNight <flukemdc@gmail.com>
2017-05-24 02:11:36 +0700

    jan's product added

--- src/js/jan.js
+++ src/js/jan.js
@@ -1,3 +1,22 @@
+
 function viewJanProduct(){
-    document.getElementById("product2").getElementsByClassName("modal-content")[0].innerHTML= "JAN";
+    // document.getElementById("product2").getElementsByClassName("modal-content")[0].innerHTML= "นอร์แนส";
+    connectXMLbyJan();
+}
+
+function connectXMLbyJan(){
+    console.log("Jan  Connection");
+    var xml = new XMLHttpRequest();
+    xml.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            var myItem = this.responseXML.getElementsByTagName("item")[1];
+            document.getElementById("product2-name").innerHTML = myItem.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+            document.getElementById("product2-desc").innerHTML = myItem.getElementsByTagName("desc")[0].childNodes[0].nodeValue;
+            document.getElementById("product2-img1").src = myItem.getElementsByTagName("img1")[0].childNodes[0].nodeValue;
+            document.getElementById("product2-img2").src = myItem.getElementsByTagName("img2")[0].childNodes[0].nodeValue;
+        }
+    }
+    xml.open("GET","src/products.xml?"+Math.random(),true);
+    xml.send();
+
 }
