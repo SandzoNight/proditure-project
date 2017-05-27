3c7e3c28e5e2a852c4808db709cbe598c1294955
SandzoNight <flukemdc@gmail.com>
2017-05-28 00:19:36 +0700

    fix file extensions for linux server #1

--- src/js/gus.js
+++ src/js/gus.js
@@ -10,7 +10,7 @@ function GetImage() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var myItem = this.responseXML.getElementsByTagName("item")[6];
+            var myItem = this.responseXML.getElementsByTagName("item")[5];
             document.getElementById("product6-name").innerHTML = myItem.getElementsByTagName("name")[0].childNodes[0].nodeValue;
             document.getElementById("product6-desc").innerHTML = myItem.getElementsByTagName("desc")[0].childNodes[0].nodeValue;
             document.getElementById("product6-img1").src = myItem.getElementsByTagName("img")[0].childNodes[0].nodeValue;
